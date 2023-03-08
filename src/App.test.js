87880bc3e61ffc243ebf1cd2f3e651da0e93d2fd
LarkAlo <118227350+LarkAlo@users.noreply.github.com>
2023-03-07 23:18:27 -0700

    Build
    the skeleton of app process

---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("it should render the title", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/bug tracker/i);
+  expect(linkElement).toBeInTheDocument();
+});
