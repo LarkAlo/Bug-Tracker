49adeab5727f0238ddf0a4569d6a50af70703149
LarkAlo <118227350+LarkAlo@users.noreply.github.com>
2023-03-08 09:32:21 -0700

    Building structure
    Modified files and added into app.js to start project. Also built path/folders structure for others

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import { useState } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
     <div>
-      <h1>🐛 Bug Tracker</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/signup">
+            <SignUp />
+          </Route>
+          <Route path="/">
+            <Sidebar />
+          </Route>
+        </Switch>
+        <Route exact path="/">
+          <Dashboard />
+        </Route>
+        <Route exact path="/user/tickets">
+          <TicketsPage />
+        </Route>
+        <Route exact path="/user/assigned">
+          <AssignedPage />
+        </Route>
+        <Route exact path="/organizations">
+          <OrganizationsPage />
+        </Route>
+        <Route exact path="/tickets/:id" />
+      </Router>
     </div>
   );
 }
-
-export default App;
